eb4792e4663e43980941483bbe6aab3113841246
hhamza1 <hamzahmitou@gmail.com>
2021-09-09 16:13:32 +0300

    Fixed email validation function

--- js/email-validation.js
+++ js/email-validation.js
@@ -4,26 +4,24 @@ var btn = document.querySelector(".btn");
 var errorMsg  = document.querySelector(".error-msg");
 
 
-var emailValidation = email.validity;
+var emailValidity = email.validity;
 
 emailValidation = (input, validator) => {
 
-    if(input.value === ""){
+    if(input.value === "" || !validator.valid){
         errorMsg.classList.add("show-msg");
         email.classList.add("red-border");
-    }
-    else if(!validator.valid){
-        errorMsg.classList.add("show-msg");
-        email.classList.add("red-border");
+
     }
     else{
         errorMsg.classList.remove("show-msg");
         email.classList.remove("red-border");
+        console.log("remove");
     }
 }
 
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    emailValidation(email, emailValidation);
+    emailValidation(email, emailValidity);
 });
